7952c8b3a7a080d91363219bdd50c35b71673880
E123WH <e123wh@mail.ru>
2021-08-15 19:01:39 +0300

    API 2.0 -- working with filesystem is in the special class

---
+++ functions/FsOperator.js
@@ -0,0 +1,89 @@
+const fs = require('fs')
+require('colors')
+
+class FileSystemOperator {
+    constructor(contentPath) {
+        this.contentPath = contentPath.replace(/\/{2,}|\/$|^\//g, '')
+    }
+    NewRequest(url) {
+        this.url = decodeURI(url).replace(/\/{2,}|\/$|^\//g, '')
+        this.elementsNumbers = {}
+        this.data = {}
+        this.getData()
+    }
+    getData() {
+        this.getLinkedFile()
+        this.getFolderList()
+        this.getAllPaths()
+        this.getNavigation()
+    }
+
+    getLinkedFile() {
+        try {
+            if (fs.statSync(`${this.contentPath}/${this.url}`).isFile()) {
+                this.data.linkedFile = {}
+                let fileMatch = this.url.match(/([^\/]*)\.([^\/]*)$/)
+                //detects full file name in .match[0]
+                //        file name without .ext in .match[1]
+                //        file extension without "." in .match[2]
+                this.data.linkedFile.name = fileMatch[1]
+                this.data.linkedFile.ext = fileMatch[2]
+                this.data.linkedFile.url = `/${this.url}`
+                this.data.linkedFile.fullUrl = `/${this.contentPath}/${this.url}`
+                this.data.currentDirectory = this.url.replace(fileMatch[0], '')
+            } else {
+                this.data.linkedFile = 'none'
+                this.data.currentDirectory = this.url
+            }
+        } catch (e) {
+            this.data.linkedFile = "Linked file not found!"
+        }
+    }
+
+    getFolderList() {
+        this.data.filesList = fs.readdirSync(`${this.contentPath}/${this.data.currentDirectory}`, {withFileTypes: true}).map(element => {
+            let name = element.name
+            let type = element.isDirectory() ? 'folder' : 'file'
+            let ext = element.isDirectory() ? null : element.name.match(/([^.]+)$/g)[0]
+            let url = encodeURI(element.name)
+            typeof this.elementsNumbers[ext] === 'undefined' ? this.elementsNumbers[ext] = 0 : null
+            let number = this.elementsNumbers[ext]++
+            return {
+                name: name,
+                type: type,
+                ext: ext,
+                url: url,
+                numberOfThisExt: number,
+            }
+        })
+    }
+
+    getAllPaths() {
+        this.data.paths = {}
+        this.data.paths.rel = `/${this.data.currentDirectory}`
+        this.data.paths.abs = `/${this.contentPath}/${this.data.currentDirectory}`
+        try {
+            this.data.paths.lvlUp = this.data.paths.rel.match(/.*\/(?=.+$)/)[0]
+        } catch (e) {
+            this.data.paths.lvlUp = null
+        }
+    }
+
+    getNavigation() {
+        let result = this.data.paths.rel.split('/')
+        result.shift()
+        result.unshift({
+            text: 'root',
+            link: '/',
+        })
+        for (let i = 1; i < result.length; i++) {
+            result[i] = {
+                text: result[i],
+                link: result[i - 1].link + result[i] + '/',
+            }
+        }
+        this.data.navigation = result
+    }
+}
+
+module.exports = FileSystemOperator
--- routes/index.js
+++ routes/index.js
@@ -1,93 +1,20 @@
 const express = require('express')
-const fs = require('fs')
-let createError = require('http-errors')
 let router = express.Router()
+const mode = 'API'
 
-//gives info about element in directory
-function elementProcessing(element, elemsNumbers) {
-    let name = element.name
-    let type = element.isDirectory() ? 'folder' : 'file'
-    let ext = element.isDirectory() ? null : element.name.match(/([^\.]+)$/g)[0]
-    let url = encodeURI(element.name)
-    typeof elemsNumbers[ext] === undefined ? elemsNumbers[ext] = '0' : null
-    let number = elemsNumbers[ext]++
-    return {
-        name: name,
-        type: type,
-        ext: ext,
-        url: url,
-        numberOfThisExt: number,
-    }
-}
+const FileSystemOperator = require('../functions/FsOperator')
+let fsOperator = new FileSystemOperator('public/content')
 
-//gets data about directory depends on path
-async function getFolderData(relPath = '') {
-    //keeps numbers of elements with any extension
-    let elemsNumbers = {}
-    let folderData = {}
-    if (fs.statSync(`./public/content/${decodeURI(relPath)}`).isFile()) {
-        console.log('FILE!!!'.red)
-        //detects full file name in .match[0]
-        //        file name without .ext in .match[1]
-        //        file extension without "." in .match[2]
-        let fileMatch = relPath.match(/([^\/]*)\.([^\/]*)$/)
-        console.log(fileMatch)
-        folderData.linkedFile = {}
-        folderData.linkedFile.name = fileMatch[1]
-        folderData.linkedFile.ext = fileMatch[2]
-        folderData.linkedFile.url = relPath
-        //relative path should be without the file
-        relPath = relPath.replace(fileMatch[0], '')
-    } else {
-        folderData.linkedFile = 'none'
-    }
-    folderData.currentFolder = 'root/' + relPath
-
-    folderData.paths = {}
-    folderData.paths.rel = ('/' + decodeURI(relPath) + '/').replace(/\/+/g, '/')
-    folderData.paths.abs = './public/content' + folderData.paths.rel
+router.get('/*', function (req, res) {
     try {
-        folderData.paths.lvlUp = folderData.paths.rel.match(/.*\/(?=.+$)/)[0]
-    } catch (e) {
-        folderData.paths.lvlUp = null
-    }
-    // console.log(`Relative: ${folderData.paths.rel}`.bgRed)
-    // console.log(`Absolute: ${folderData.paths.abs}`.bgBlue)
-    // console.log(`Level Up: ${folderData.paths.lvlUp}`.bgGreen)
-    folderData.ls = []
-    //creating list of elements
-    folderData.ls = fs.readdirSync(folderData.paths.abs, {withFileTypes: true}).map(element => elementProcessing(element, elemsNumbers))
-    folderData.navigation = parseNavigation(folderData.paths.rel)
-    return folderData
-}
-
-//adds items for navigation bar
-function parseNavigation(path) {
-    let result = path.split('/')
-    result.pop()
-    result.shift()
-    result.unshift({
-        text: 'root',
-        link: '/',
-    })
-    for (let i = 1; i < result.length; i++) {
-        result[i] = {
-            text: result[i],
-            link: result[i - 1].link + result[i] + '/',
+        fsOperator.NewRequest(req.params[0])
+        if (mode === 'API') {
+            res.send(fsOperator.data)
+        } else if (mode === 'site') {
+            res.render('folder', fsOperator.data)
         }
-    }
-    return result
-}
-
-//res.render is for site mode
-//res.send is for API mode
-router.get('/*', async function (req, res, next) {
-    console.log(req.params)
-    try {
-        //await res.render('folder', await getFolderData(req.params[0]))
-        await res.send(await getFolderData(req.params[0]))
     } catch (e) {
-        await res.send('error')
+        res.send('error')
     }
 })
 
