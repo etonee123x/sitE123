454d238014eabd349d8fa2663ecdb21d54f125b0
E123WH <e123wh@mail.ru>
2022-01-08 10:54:35 +0300

    testing os interfaces

--- bin/www.js
+++ bin/www.js
@@ -5,13 +5,15 @@ const port = normalizePort(process.env.PORT || '3001');
 app.set('port', port);
 const server = http.createServer(app);
 server.once('listening', () => {
+    var _a, _b;
     const net = networkInterfaces();
-    for (const iface in net) {
-        if (Object.prototype.hasOwnProperty.call(net, iface)) {
-            console.log(iface);
+    let ip;
+    for (const _interfaceTitle in net) {
+        if (Object.prototype.hasOwnProperty.call(net, _interfaceTitle) && !ip) {
+            ip = (_b = (_a = net[_interfaceTitle]) === null || _a === void 0 ? void 0 : _a.find(_interface => !_interface.internal && _interface.family === 'IPv4')) === null || _b === void 0 ? void 0 : _b.address;
         }
     }
-    console.log(`Server have been launched at http://localhost:${port}`);
+    console.log(`Server have been launched at http://${ip || 'localhost'}:${port}`);
 });
 server.listen(port);
 function normalizePort(val) {
--- bin/www.ts
+++ bin/www.ts
@@ -8,12 +8,15 @@ app.set('port', port);
 const server = http.createServer(app);
 server.once('listening', () => {
   const net = networkInterfaces();
-  for (const iface in net) {
-    if (Object.prototype.hasOwnProperty.call(net, iface)) {
-      console.log(iface);
+  let ip;
+  for (const _interfaceTitle in net) {
+    if (Object.prototype.hasOwnProperty.call(net, _interfaceTitle) && !ip) {
+      ip = net[_interfaceTitle]?.find(
+        _interface => !_interface.internal && _interface.family === 'IPv4',
+      )?.address;
     }
   }
-  console.log(`Server have been launched at http://localhost:${port}`);
+  console.log(`Server have been launched at http://${ip || 'localhost'}:${port}`);
 });
 
 server.listen(port);
