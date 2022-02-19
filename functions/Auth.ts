7a942c4e7d1eb6bb401c1c5cb128fb7ccc5562a9
E123WH <e123wh@mail.ru>
2022-02-19 15:56:57 +0300

    trying auth operations...

---
+++ functions/Auth.ts
@@ -0,0 +1,52 @@
+import('dotenv/config');
+import { Response } from 'express';
+
+import { sign, verify, JwtPayload } from 'jsonwebtoken';
+
+export default class Auth {
+  private login?: string;
+  private password?: string;
+  private token?: string;
+  private verifyIsLost = false;
+
+  constructor({
+    login,
+    password,
+    token,
+  }: {
+    login?: string;
+    password?: string;
+    token?: string;
+  }) {
+    const getLoginByToken = (token: string) => {
+      let login;
+      verify(token, process.env.THE_KEY as string, (error, payload) => {
+        if (error)
+         return this.verifyIsLost = true;
+        if (payload && (payload as JwtPayload).login)
+          login = (payload as JwtPayload).login;
+      });
+      return login;
+    };
+    if (token) {
+      this.token = token;
+      this.login = getLoginByToken(token);
+    } else {
+      this.login = login;
+      this.password = password;
+    }
+  }
+
+  public tryAuth(res: Response) {
+    if (this.token) {
+      if (!this.verifyIsLost) return res.json(this.token);
+      return res.sendStatus(403);
+    }
+    if (!(this.login && this.password)) return res.sendStatus(404);
+    if (this.password === process.env[this.login])
+      return res.json(
+        sign({ login: this.login }, process.env.THE_KEY as string),
+      );
+    return res.sendStatus(403);
+  }
+}
