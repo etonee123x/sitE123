24d71a60325fa481f42a333c0edd36dedb8168fe
Evgeniy Uvarov <etonee123x@gmail.com>
2024-06-24 10:57:17 +0400

    upd promises

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,27 @@ import https from 'https';
 import { app } from '@/app';
 import { logger } from '@/helpers/logger';
 
-const pathToCert = String(process.env.PATH_TO_CERT);
-const pathToKey = String(process.env.PATH_TO_KEY);
+const pathToCert = process.env.PATH_TO_CERT;
+const pathToKey = process.env.PATH_TO_KEY;
 
-Promise.all([
-  access(pathToCert).then(() => String(readFile(pathToCert))),
-  access(pathToKey).then(() => String(readFile(pathToKey))),
-]).then(async ([cert, key]) => {
-  https
-    .createServer({ key, cert }, app)
-    .once('listening',
-      () => logger(`HTTPS server is listening on https://${process.env.DOMAIN_NAME}:${process.env.PORT_HTTPS}`),
-    )
-    .listen(process.env.PORT_HTTPS)
-    .on('error', (error) => logger.error('Failed to start HTTPS server due to:', error));
-}).catch((e) => {
-  logger.error('Failed to start HTTPS server due to:', e);
-});
+if (pathToCert && pathToKey) {
+  Promise.all([
+    access(pathToCert).then(() => readFile(pathToCert, 'utf-8')),
+    access(pathToKey).then(() => readFile(pathToKey, 'utf-8')),
+  ]).then(async ([cert, key]) => {
+    https
+      .createServer({ key, cert }, app)
+      .once('listening',
+        () => logger(`HTTPS server is listening on https://${process.env.DOMAIN_NAME}:${process.env.PORT_HTTPS}`),
+      )
+      .listen(process.env.PORT_HTTPS)
+      .on('error', (error) => logger.error('Failed to start HTTPS server due to:', error));
+  }).catch((e) => {
+    logger.error('Failed to start HTTPS server due to:', e);
+  });
+} else {
+  logger('HTTPS server was not started because the certificate paths are missing');
+}
 
 http
   .createServer(app)
